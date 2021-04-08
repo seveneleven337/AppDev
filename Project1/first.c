6a80444dfa86a4c25bb0d27e1b96d91e7ff97345
e2001875 <e2001875@edu.vamk.fi>
2021-04-08 12:00:48 +0300

    first version

---
+++ Project1/first.c
@@ -0,0 +1,28 @@
+// this program will take 2 positive integers from the user, and display
+// all numbers dibisible by 3 between these two integers
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	printf("Enter 2 positive integers: ");
+	scanf("%d%d", &a, &b);
+	if (a > b) {
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	for (int i=a, c=0; i<= b; i++){
+		if (i % 3 == 0) {
+			printf("%6d", i);
+			c += 1;
+			if (c % 10 == 0) {
+				printf("\n");
+			}
+		}
+	}
+	return 0;
+}
